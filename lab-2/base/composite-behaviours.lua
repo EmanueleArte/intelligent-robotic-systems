c39cb712bc49b23b5c41d0b354eb06ec700cf879
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2025-03-07 17:03:30 +0100

    Fix

--- lab-2/base/composite-behaviours.lua
+++ lab-2/base/composite-behaviours.lua
@@ -16,7 +16,7 @@ PROX_THRESHOLD = 0.1
 
 n_steps = 0
 random_rotation = 0
-speed = 0
+n_ignore = 0
 multiplier = 1
 
 --[[ This function is executed every time you press the 'execute'
@@ -26,7 +26,7 @@ function init()
 	right_v = MAX_VELOCITY
 	robot.wheels.set_velocity(left_v,right_v)
 	n_steps = UNSTUCK_STEPS
-	speed = MAX_VELOCITY
+	n_ignore = 5
 	robot.leds.set_all_colors("black")
 end
 
@@ -40,25 +40,25 @@ function step()
 
 	-- Unstuck
 	if n_steps < UNSTUCK_STEPS then
-		robot.wheels.set_velocity(-MAX_VELOCITY,-MAX_VELOCITY)
-		-- robot.wheels.set_velocity(-MAX_VELOCITY,0)
+		-- robot.wheels.set_velocity(-MAX_VELOCITY,-MAX_VELOCITY)
+		robot.wheels.set_velocity(-MAX_VELOCITY,0)
 		n_steps = n_steps + 1
 		random_rotation = robot.random.uniform_int(UNSTUCK_STEPS,UNSTUCK_STEPS * 3)
 		return
 	end
-	if n_steps < UNSTUCK_STEPS + random_rotation then
-		robot.wheels.set_velocity(-MAX_VELOCITY,0)
-		n_steps = n_steps + 1
-		return
-	end
+	-- if n_steps < UNSTUCK_STEPS + random_rotation then
+	-- 	robot.wheels.set_velocity(-MAX_VELOCITY,0)
+	-- 	n_steps = n_steps + 1
+	-- 	return
+	-- end
 
 	for i=1,#robot.proximity do
 		--[[ Sum readings for left and right proximity sensors ]]
 		if robot.proximity[i].value > PROX_THRESHOLD + min(multiplier, 0.6) then
-			if i >= 19 and i <= 24 then
-				left_prox = left_prox + robot.proximity[i].value
-			elseif i >= 1 and i <= 6 then
+			if i >= 20 and i <= 24 then
 				right_prox = right_prox + robot.proximity[i].value
+			elseif i >= 1 and i <= 5 then
+				left_prox = left_prox + robot.proximity[i].value
 			end
 		end
 	end
@@ -67,19 +67,19 @@ function step()
 	-- Action based on left proximity readings
 	if left_prox > 0 then
 		robot.leds.set_all_colors("red")
-		left_v = -left_prox * 2
-		-- right_v = 0
+		right_v = -left_prox
 		stuck = stuck + 1
 		multiplier = 0.3
+		n_ignore = 0
 	end
 
 	-- Action based on right proximity readings
 	if right_prox > 0 then
 		robot.leds.set_all_colors("red")
-		-- left_v = 0
-		right_v = -right_prox * 2
+		left_v = -right_prox
 		stuck = stuck + 1
 		multiplier = 0.3
+		n_ignore = 0
 	end
 
 	-- If stuck becuase of both proximity sensors detect obstacles
@@ -89,7 +89,7 @@ function step()
 	end
 
 	-- Go straight if no obstacles detected
-	if left_prox == 0 and right_prox == 0 then
+	if stuck == 0 then
 		-- Phototaxis
 		light = false
 		sum = 0
@@ -110,7 +110,7 @@ function step()
 
 		multiplier = multiplier + 0.1
 
-		if light == true then
+		if light == true and n_ignore > UNSTUCK_STEPS then
 			robot.leds.set_all_colors("yellow")
 			-- [[ Check if light in front is increasing or decreasing and move accordingly ]]
 			if max_i == 1 or max_i == 24 then
@@ -131,7 +131,7 @@ function step()
 	end
 
 	robot.wheels.set_velocity(left_v,right_v)
-	-- log("left_v: " .. left_v .. " right_v: " .. right_v)
+	n_ignore = n_ignore + 1
 
 end
 
@@ -147,6 +147,7 @@ function reset()
 	right_v = robot.random.uniform(0,MAX_VELOCITY)
 	robot.wheels.set_velocity(left_v,right_v)
 	n_steps = 0
+	n_ignore = 5
 	robot.leds.set_all_colors("black")
 end
 
